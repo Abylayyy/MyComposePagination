36b3d3b20dcc8a95b621a8c0801ec33a05f97834
abylay_tillabek <abylay.tillabek@gmail.com>
2023-03-14 16:50:11 +0600

    [compose] Drawn profile image

--- app/src/main/java/kz/abylay/mypaginationapp/ui/navigation/NavigationDrawer.kt
+++ app/src/main/java/kz/abylay/mypaginationapp/ui/navigation/NavigationDrawer.kt
@@ -1,29 +1,40 @@
 package kz.abylay.mypaginationapp.ui.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kz.abylay.mypaginationapp.R
 
 @Composable
 fun DrawerHeader() {
-    Box(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 64.dp),
-        contentAlignment = Alignment.Center
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Header", fontSize = 60.sp)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Profile image",
+            modifier = Modifier.clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Text(text = "Abylay Tillabek", fontSize = 30.sp)
     }
 }
 
